40f095b5a7cfe1b6dbd286ff345463ab910487a1
AntiTriki <ne.i.on@hotmail.com>
2017-06-17 21:00:37 -0400

    Empezando a correr el Layout, agarra las categorias

--- PComercio EF final/PlantillaComercio/Controllers/EventoController.cs
+++ PComercio EF final/PlantillaComercio/Controllers/EventoController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AccesoDatos.Domain.Services;
+using AccesoDatos.Infrastructure.Data.DataModels;
+using PagedList;
+using PagedList.Mvc;
 
 namespace PlantillaComercio.Controllers
 {
     public class EventoController : Controller
     {
+        readonly EventoServicio _paisServicio = new EventoServicio();
+        ejmEntities4 db = new ejmEntities4();
         // GET: Evento
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetEventos(string term)
+        {
+            List<string> prodNames = db.evento_empresa.Where(x => x.nombre.StartsWith(term)).Select(y => y.nombre).ToList();
+            return Json(prodNames, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult GetEventosByCategory(string categorynombre, int? page)
+        {
+            ViewBag.Categories = db.categoria_evento.Select(x => x.nombre).ToList();
+            
+
+            
+
+            var prods = db.evento_empresa.Where(x => x.categoria_evento.nombre == categorynombre).ToList();
+            return View("Products", prods.ToPagedList(page ?? 1, 9));
         }
     }
 }
